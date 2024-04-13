3fe9ecf71ede8650dbe6894edd615c1bb274005a
Newton Bofulo <newtonombese1@gmail.com>
2024-04-13 07:13:51 +0300

    Setting up the basic homepage

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='text-3xl bg-[url("https://images.ctfassets.net/lmx885sr3per/2bDHgclAb2HmbBRNUuDscD/d8c64acd6f2c7ee6eed8577c69d8d7ee/hero_desktop.webp")] h-screen bg-cover'>
-      Home
+    <div className='bg-[url("https://images.ctfassets.net/lmx885sr3per/2bDHgclAb2HmbBRNUuDscD/d8c64acd6f2c7ee6eed8577c69d8d7ee/hero_desktop.webp")] h-screen bg-cover'>
+      <h3>Home</h3>
     </div>
   )
 }
